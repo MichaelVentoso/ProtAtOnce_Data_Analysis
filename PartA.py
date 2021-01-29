e953e70f77cf57dfd2c6ab043f84118137e00923
MichaelVentoso <67383434+MichaelVentoso@users.noreply.github.com>
2021-01-29 00:27:59 -0500

    Add files via upload

--- PartA.py
+++ PartA.py
@@ -13,6 +13,7 @@ import sys
 # Libs
 import numpy
 import pandas
+from scipy import stats
 import matplotlib.pyplot
 
 """
@@ -102,8 +103,8 @@ def ageHistogram(df):
     matplotlib.pyplot.ylabel("Number of NBA Players")
 
     #displays and saves histogram
-    matplotlib.pyplot.show()
     matplotlib.pyplot.savefig("Age_histogram.png")
+    matplotlib.pyplot.show()
 
 
 #Returns the average age of all players in given dataframe (rounded to two decimal places)
@@ -167,7 +168,7 @@ def averageAgeOfTeams(df):
 
     #print raw data and line for spacing
     print("There are",num_teams_below, "teams with an average team age lower than the average age of all NBA players.")
-    print("There are", num_teams_above, "teams with an average team age higher than the average age of all NBA players.")
+    print("There are", num_teams_above, "teams with an average team age higher or equal to the average age of all NBA players.")
     print()
 
     #save dictionary with teams and average team age's to file, to avoid a large print to console
@@ -198,7 +199,9 @@ def ageOfTopShooters(df):
     #selects only the top 10 shooters that meet the available criteria
     top_shooters = double_filtered_df.nlargest(10,'FG.')
 
+
     #creates scatterplot, and vertical line showing the average NBA player's age for reference
+    matplotlib.pyplot.clf()
     top_shooters.plot.scatter(y='FG.', x='Age')
     matplotlib.pyplot.axvline(x=average_age)
 
@@ -208,8 +211,71 @@ def ageOfTopShooters(df):
     matplotlib.pyplot.ylabel("Percentage of Field Goals Made")
 
     #displays and saves the scatterplot
-    matplotlib.pyplot.show()
     matplotlib.pyplot.savefig("TopShooters.png")
+    matplotlib.pyplot.show()
+
+
+"""
+Since it seemed the top shooters from the previous figure were 
+not all from the same age group, I predict:
+
+Hypothesis: There will be no correlation between the age of the 
+player and how well they are at shooting free throws.
+
+This time, I want to analyze more than the top shooters, but in order to 
+remove outliers I will limit the dataset to only players who have attempted
+at least 100 field goals.
+
+The statistical tests used in this function:
+    Anderson-Darling Normality Test
+    Pearson Correlation Test
+
+This function:
+Determines if the data to analyze is normally distributed
+If not, prints error message and returns
+If data is normally distributed, performs correlation test
+Prints related values:
+    r (correlation value)
+    p (two-tailed p value)
+Creates/saves scatterplot of data
+"""
+def ageToFGCorrelation(df):
+
+    #reduces large dataframe to include only the necessary columns
+    #filters out players who have attempted less than 100 fg attempts
+    #also removes rows that have empty data for any of those columns
+    simplified_df = df[['Age', 'FG.', 'FGA']].copy()
+    filtered_df = simplified_df[simplified_df['FGA'] >= 100]
+    filtered_df = filtered_df.dropna()
+
+    #performs normality test
+    anderson_results_age = stats.anderson(filtered_df['Age'], dist='norm')
+    anderson_results_fg = stats.anderson(filtered_df['FG.'], dist='norm')
+
+    #if data is not normal, prints error message and returns
+    if anderson_results_age[0] < .05 or anderson_results_fg[0] < .05:
+        print("The data received does not pass the Anderson-Darling test for normal distribution.")
+        print("Because of this, we cannot perform further statistical tests on this data.")
+        return
+
+    #performs correlation test on data and prints relevant values
+    pearson_results = stats.pearsonr(filtered_df['Age'], filtered_df['FG.'])
+    print("The Pearson Correlation test shows an r-value of" , pearson_results[0])
+    print("and a p-value of ", pearson_results[1],".")
+    print()
+
+    # creates scatterplot
+    matplotlib.pyplot.clf()
+    filtered_df.plot.scatter(y='FG.', x='Age')
+
+    # sets labels for plot
+    matplotlib.pyplot.suptitle("Age vs. % of Field Goals Made")
+    matplotlib.pyplot.xlabel("Age")
+    matplotlib.pyplot.ylabel("Percentage of Field Goals Made")
+
+    # displays and saves the scatterplot
+    matplotlib.pyplot.savefig("Age_fg_correlation.png")
+    matplotlib.pyplot.show()
 
 
 """
@@ -220,7 +286,8 @@ if __name__ == '__main__':
     print("STARTED")
     print()
     nba_dataframe = createCombinedDataFrame()
-    #ageHistogram(nba_dataframe)
-    #averageAgeOfTeams(nba_dataframe)
-    #ageOfTopShooters(nba_dataframe)
+    ageHistogram(nba_dataframe)
+    averageAgeOfTeams(nba_dataframe)
+    ageOfTopShooters(nba_dataframe)
+    ageToFGCorrelation(nba_dataframe)
     print("FINISHED")
